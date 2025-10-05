9997dc346c6923261ca8d7c5900fcd5ddad3a7c0
Rxbru <amarmahato.cs@gmail.com>
2025-10-05 11:23:14 +0530

    userinit file

--- backend/src/config/supabase.config.ts
+++ backend/src/config/supabase.config.ts
@@ -6,4 +6,4 @@ const supabaseUrl = process.env.SUPABASE_URL as string;
 const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY as string;
 // const supabasePublishableKey = process.env.SUPABASE_PUBLISHABLE_KEY as string;
 
-export const supabase = createClient(config.supabase.url, config.supabase.secretKey);
+export const supabaseAdmin = createClient(config.supabase.url, config.supabase.secretKey);
